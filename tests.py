dd869e8d5d07ddc787c726b297106359d69ac776
ivys22 <98072858+ivys22@users.noreply.github.com>
2024-04-05 04:41:54 +0000

    Unit tests for a series of data processing functions.

---
+++ tests.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from assets import raw_comments, preprocessed_comments, sentiment_analysis, sentiment_summary
+from unittest.mock import patch
+from unittest.mock import Mock
+
+def test_raw_comments():
+    """Tests the raw_comments function."""
+    ...
+
+def test_preprocessed_comments():
+    """Tests the preprocessed_comments function."""
+    ...
+
+def test_sentiment_analysis():
+    """Tests the sentiment_analysis function."""
+    ...
+
+def test_sentiment_summary():
+    """Tests the sentiment_summary function."""
+    ...
