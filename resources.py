786f26a1c90e239267c6f6b2cc5c316591068969
ivys22 <98072858+ivys22@users.noreply.github.com>
2024-04-05 02:00:33 +0000

    Conducts text preprocessing for NLP tasks.

---
+++ resources.py
@@ -0,0 +1,20 @@
+from dagster import resource, Field
+import re
+import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+
+nltk.download('stopwords')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+
+class TextPreprocessor:
+    """Defines a TextPreprocessor class that preprocesses text data to prepare it for NLP tasks."""
+    def __init__(self, remove_stopwords=True):
+       ...
+    def preprocess(self, text):
+       ...
+
+@resource(config_schema={"remove_stopwords": Field(bool, is_required=False, default_value=True)})
+def text_preprocessor_resource(init_context):
+    """Defines a Dagster resource for the TextPreprocessor class, making it possible to integrate this text preprocessing utility within a Dagster pipeline."""
