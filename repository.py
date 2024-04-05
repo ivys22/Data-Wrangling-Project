1d05326278afc185c1c005c3d63db0780b568c9c
ivys22 <98072858+ivys22@users.noreply.github.com>
2024-04-05 04:21:26 +0000

    Defines a repository which serves as a container for organizing and managing assets and resources for a data processing pipeline focused on mental health sentiment analysis.

--- repository.py
+++ repository.py
@@ -5,4 +5,7 @@ from resources import text_preprocessor_resource
 @repository
 def mental_health_repo():
     """Defines a repository which serves as a container for organizing and managing assets and resources for a data processing pipeline focused on mental health sentiment analysis."""
-    ...
+    return {
+        "assets": [raw_comments, preprocessed_comments, sentiment_analysis, sentiment_summary],
+        "resources": {"text_preprocessor": text_preprocessor_resource},
+    }
