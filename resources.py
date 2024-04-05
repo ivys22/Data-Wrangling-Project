47920bc95a90224ac142f05b9b77c14fcc82f026
ivys22 <98072858+ivys22@users.noreply.github.com>
2024-04-05 02:06:02 +0000

    Determine whether stopwords should be removed during preprocessing.

--- resources.py
+++ resources.py
@@ -11,10 +11,13 @@ nltk.download('omw-1.4')
 class TextPreprocessor:
     """Defines a TextPreprocessor class that preprocesses text data to prepare it for NLP tasks."""
     def __init__(self, remove_stopwords=True):
-       ...
+        self.remove_stopwords = remove_stopwords
+        self.lemmatizer = WordNetLemmatizer()
+        self.stopwords = set(stopwords.words('english'))
     def preprocess(self, text):
-       ...
+        ...
 
 @resource(config_schema={"remove_stopwords": Field(bool, is_required=False, default_value=True)})
 def text_preprocessor_resource(init_context):
     """Defines a Dagster resource for the TextPreprocessor class, making it possible to integrate this text preprocessing utility within a Dagster pipeline."""
+    ...
